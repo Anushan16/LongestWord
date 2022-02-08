7d2e3d67fa888a78e793770e66dff8f9bdddaea4
Anushan R <anushanr@Anushans-Mac-mini.local>
2022-02-08 13:33:48 -0500

    Added line comments

--- LongestWord.py
+++ LongestWord.py
@@ -1,11 +1,18 @@
 import re
 highV = "" # o(1)
 def LongestWord(sen):
+  #use RegEx to remove all special characters from string 
   newString = re.sub('[^a-zA-Z0-9 \n\.]', '', sen) # 0(n)
+  #use the split method to turn sen into a array, called nArray
   nArray = newString.split() # 0(n)
+  # initiate a variable to keep the element with the longest length
+  highV = "" # o(1)
+  #loop through each element in nArray 
   for e in nArray: # 0(n)
+    #if current element is greater that the len of highV... highV = e (current element)
     if len(e) > len(highV): # 0(1)
       highV = e # 0(1)
+  # at the end of the for loop return highV
   return highV # 0(1)
   
   
